f2b914fc0bc3a49f72fa544fb89baf586114245b
Neelambh <neelambhagat1947@gmail.com>
2025-03-19 00:54:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+   region = ap-south-1
+}
+
+terraform {
+backend "s3" {
+   bucket = "bucket-name"
+   key = " "  # it is the path where state file is stored 
+   region = 
+   dyname_db_table = ""  # dynamo db table for state locking
+   encrypt = true
+}
+}
 
